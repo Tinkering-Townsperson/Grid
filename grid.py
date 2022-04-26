17476ccaa9a9b51228f92d2f905e738c102496f2
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-04-25 21:15:32 -0700

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,12 +1,16 @@
 import os, sys
 file = sys.argv[1]
 print("It looks like you tried to open \""+file+"\".")
+if not os.system("echo \"%path:; C:\\Program Files (x86)\\Grid\\=%\"") == os.system("\"%path%\""):
+    print("YAY!!!")
+else:
+    os.system("set path=%path%; C:\\Program Files (x86)\\Grid\\")
 op = input("""Would you like to open it in:
 1. Compile the file(default)
 2. Edit the file
 """)
 
 if op == "2":
-    os.system("'C:\\Program Files (x86)\\Grid\\gride.exe' '"+file+"'")
+    os.system("gride.exe \""+file+"\"")
 else:
-    os.system("'C:\\Program Files (x86)\\Grid\\gridc.exe' '"+file+"'")
+    os.system("gridc.exe \""+file+"\"")
