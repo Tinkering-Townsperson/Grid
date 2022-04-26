b218fa00639897cd73d83a1cf788a3036322a4c6
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-04-26 13:15:32 -0700

    Added .gitignore and fixed grid.py

--- grid.py
+++ grid.py
@@ -1,16 +1,12 @@
 import os, sys
 file = sys.argv[1]
 print("It looks like you tried to open \""+file+"\".")
-if not os.system("echo \"%path:; C:\\Program Files (x86)\\Grid\\=%\"") == os.system("\"%path%\""):
-    print("YAY!!!")
-else:
-    os.system("set path=%path%; C:\\Program Files (x86)\\Grid\\")
-op = input("""Would you like to open it in:
+op = input("""Would you like to:
 1. Compile the file(default)
-2. Edit the file
+2. Edit the file(Work In Progress)
 """)
 
 if op == "2":
-    os.system("gride.exe \""+file+"\"")
+    os.system("C:\\Grid\\gride.exe "+file)
 else:
-    os.system("gridc.exe \""+file+"\"")
+    os.system("C:\\Grid\\gridc.exe "+file)
