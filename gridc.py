92fe141cad1e1cd21794ce70ff83c7bcd355e3d0
tinkering-townsperson <h2o.Drop2010@gmail.com>
2022-04-26 18:21:27 -0700

    Update gridc.py

--- gridc.py
+++ gridc.py
@@ -1,12 +1,14 @@
 from os.path import exists
 from PIL import Image, ImageDraw
+from datetime import datetime as dt
 import sys, re
 
+print("Starting...")
 imgpath=gridpath = None#path to file, from sys.argv[1]
-grid = 0#file
-fn=ft=ct=w=h=None#grid attributes, such as name, filetype, colortype, width, and height, respectively
+log=grid = None#file
+fn=ft=ct=w=h = None#grid attributes, such as name, filetype, colortype, width, and height, respectively
 iso = re.compile(r'[a-zA-Z0-9]+')#regexp for isolating letters and numbers
-c = 0#temporary var for the curent regexp
+c = 0#temporary var for the current regexp
 gdata = 0#data inside file
 if len(sys.argv) == 2:
     gridpath = sys.argv[1]
@@ -16,19 +18,26 @@ else:
     print("Looks like you forgot to pass a filepath!")
 if exists(gridpath):
     grid = open(gridpath)
-    print(grid)
+    log = open("C:\\Grid\\Grid.log", mode='at')
+    log.write("Start time:"+dt.now().strftime("%b-%d-%Y %H:%M:%S\n"))
+    log.write("File:"+gridpath + "\nFile descriptor:" + grid + "\nFile data:")
+    #print(grid)
     gdata =grid.read()
-    print(gdata + "\n")
+    log.write(gdata + "\nFilename:")
+    #print(gdata + "\n")
     c = re.findall(r': .+\s', gdata)
     fn = iso.findall(c[0])[0]
     ft = iso.findall(c[1])[0]
     ct = iso.findall(c[2])[0]
-    print(fn+"."+ft,"\ncolortype:",ct)
+    log.write(fn + "." + ft + "\ncolortype:" + ct + "\nGrid data(pixel art):")
+    #print(fn+"."+ft,"\ncolortype:",ct)
     c = re.findall(r':START\n([\d\w\s]+)\n:END', gdata)[0]
-    print(c.split("\n"),"\n")
+    log.write(c.split("\n") + "\n(X, Y, Width, Height):")
+    #print(c.split("\n"),"\n")
     w = len(c.split("\n")[0])
     h = len(c.split("\n"))
-    print(c, (w,h))
+    log.write(c + (w,h) + "\n")
+    #print(c, (w,h))
     if ct == "grayscale":
         im = Image.new(mode="RGB", size=(w*100,h*100))
         draw = ImageDraw.Draw(im)
@@ -36,7 +45,8 @@ if exists(gridpath):
             for x in range(w):
                 if c.split("\n")[y][x] == "1":
                     draw.rectangle((x*100, y*100, x*100+100, y*100+100), fill=(0, 0, 0))
-                    print(x, y, w, h)
+                    log.write((x, y, w, h))
+                    #print(x, y, w, h)
                 else:
                     draw.rectangle((x*100, y*100, x*100+100, y*100+100), fill=(255, 255, 255))
         im.save(imgpath+fn+'.'+ft, quality=95)
@@ -45,3 +55,5 @@ if exists(gridpath):
     grid.close()
 else:
     print("File doesn\'t exist!")
+log.write("End time:"+dt.now().strftime("%b-%d-%Y %H:%M:%S\n"))
+log.close()
